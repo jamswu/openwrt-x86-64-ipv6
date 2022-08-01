7c0d256899787b7e1939bb0066f5f4c5a81e7932
jamswu <136735195@qq.com>
2022-08-01 14:45:32 +0800

    Update loadpackets.sh

--- loadpackets.sh
+++ loadpackets.sh
@@ -1,4 +1,4 @@
-echo "src-git opentopd  https://github.com/sirpdboy/sirpdboy-package" >>feeds.conf.default
+echo "src-git opentopd  https://github.com/jamswu/sirpdboy-package" >>feeds.conf.default
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >>feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small" >>feeds.conf.default
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
