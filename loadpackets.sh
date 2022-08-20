257b97992708b2360edbeea8179a6fb0993f9580
jamswu <jamswu@test.com>
2022-08-20 12:13:43 +0800

    modify workflows

--- loadpackets.sh
+++ loadpackets.sh
@@ -1,6 +1,6 @@
-#echo "src-git opentopd  https://github.com/sirpdboy/sirpdboy-package" >>feeds.conf.default
+echo "src-git opentopd  https://github.com/sirpdboy/sirpdboy-package" >>feeds.conf.default
 echo "src-git kenzo https://github.com/kenzok8/openwrt-packages" >>feeds.conf.default
 echo "src-git small https://github.com/kenzok8/small" >>feeds.conf.default
-git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package
-rm -rf package/sirpdboy-package/luci-app-poweroffdevice
+#git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package
+#rm -rf package/sirpdboy-package/luci-app-poweroffdevice
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
